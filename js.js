7277cc629a1a8efb3e71e4979c92afc0accf795c
Sage Lawrence <sageblawrence@gmail.com>
2022-10-25 19:10:02 -0700

    Added basic logic for a single round win/loss/tie

--- js.js
+++ js.js
@@ -3,14 +3,41 @@ function getComputerChoice() {
     let randVal = Math.floor(Math.random()*3)+1;
 
     if (randVal === 1) {
-        computerSelection = 'rock';
+        computerChoice = 'rock';
     }
     else if (randVal === 2) {
-        computerSelection = 'paper';
+        computerChoice = 'paper';
     }
     else {
-        computerSelection = 'scissors';
+        computerChoice = 'scissors';
     }
-    return(computerSelection);
+    return(computerChoice);
 
 } // generates a choice for the player to play against
+
+const choices = document.querySelectorAll('.choice');
+
+function playRound() { // runs onclick from arrow function below
+
+    let userChoice = this.id; // sets the id of the image selected as the choice
+    
+    let computerChoice = getComputerChoice();
+    
+    if (userChoice === computerChoice) {
+
+       console.log('Tie');
+
+    } else if (userChoice === 'rock' && computerChoice === 'scissors'
+    || userChoice === 'paper' && computerChoice === 'rock'
+    || userChoice === 'scissors' && computerChoice === 'paper') {
+
+        console.log('Win');
+
+    } else {
+        
+        console.log('Loss');
+
+    }
+}
+
+choices.forEach(choice => choice.addEventListener('click', playRound)); // adds click listener to pics
